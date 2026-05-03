f4c60e18b53f4523730d33bc92a12111d1d58610
Joyash Maharjan <Joyashmrzn@gmail.com>
2026-05-03 21:45:04 +0545

    fix: use opencv headless for Railway

--- backend/settings.py
+++ backend/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%5(^jfs0fthv_rjco6@77_%*duaolz*do7r(r46bqv-b%jr!na'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
