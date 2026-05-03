5c5e919259cabed573520ae9567258d2c3be6113
Joyash Maharjan <Joyashmrzn@gmail.com>
2026-05-03 20:58:46 +0545

    fix CORS setting name

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ORIGIN_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
