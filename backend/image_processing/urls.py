1d70d9817a625fbde7ac652c7cc447230b9d4cb6
Joyash Maharjan <Joyashmrzn@gmail.com>
2026-05-03 22:02:32 +0545

    fix: remove download_image from urls

--- backend/image_processing/urls.py
+++ backend/image_processing/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import upload_passport
 
 urlpatterns = [
-    path('process-image/', upload_passport),
-    
+    path('process-image/', upload_passport, name='upload_passport'),
 ]
